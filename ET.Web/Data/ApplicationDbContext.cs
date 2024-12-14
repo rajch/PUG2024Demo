0e7c2acc7048a45876b75fe8119ef8e59644ebf8
Raj Chaudhuri <rajch@hotmail.com>
2024-12-14 12:22:45 +0530

    Added Data Protection to DbContext, migrations and manifest

--- ET.Web/Data/ApplicationDbContext.cs
+++ ET.Web/Data/ApplicationDbContext.cs
@@ -2,12 +2,15 @@ using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using ET.Web.Models;
 using Microsoft.Net.Http.Headers;
+using Microsoft.AspNetCore.DataProtection.EntityFrameworkCore;
 
 namespace ET.Web.Data;
 
-public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+public class ApplicationDbContext : IdentityDbContext<ApplicationUser>, IDataProtectionKeyContext
 {
     public DbSet<Expense> Expenses => Set<Expense>();
+
+    public DbSet<DataProtectionKey> DataProtectionKeys => Set<DataProtectionKey>();
 
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
         : base(options)
--- ET.Web/Program.cs
+++ ET.Web/Program.cs
@@ -3,6 +3,7 @@ using ET.Web.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.AspNetCore.DataProtection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -43,6 +44,10 @@ builder.Services.AddHealthChecks()
                     tags: ["ready"]
                 )
 ;
+
+// Add Data Protection, saved to DbContext
+builder.Services.AddDataProtection()
+    .PersistKeysToDbContext<ApplicationDbContext>();
 
 // Add Razor Pages
 
